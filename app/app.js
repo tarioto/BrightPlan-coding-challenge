8fe222613fd666d38174cc3f0c6f778446245093
Tim Arioto <timarioto@gmail.com>
2017-03-03 15:55:15 -0800

    bootstrap testing

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import 'sanitize.css/sanitize.css';
-
 import App from './containers/App';
 
 import configureStore from './store';
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -21,32 +21,47 @@ import { PageHeader, DropdownButton, Grid, Row, Col, Clearfix, Panel } from 'rea
 
 class App extends React.Component {
   render () {
-    return <div>
-      <PageHeader />
-      <Grid>
-        <Row className="show-grid">
-          <Col xs={12} sm={12} md={6} lg={6}><Panel header={'test'}>Look I'm in a well!</Panel></Col>
-          <Col xs={12} sm={12} md={6} lg={6}><Panel>Look I'm in a well!</Panel></Col>
-          {/* <Clearfix visibleSmBlock><code>&lt;{'Clearfix visibleSmBlock'} /&gt;</code></Clearfix>
-          <Col sm={6} md={3}><Panel>Look I'm in a well!</Panel></Col>
-          <Col sm={6} md={3}><Panel>Look I'm in a well!</Panel></Col> */}
-        </Row>
-      </Grid>
+    return <div className="container-fluid">
 
-      <DropdownButton />
-      <div className="container" >
-      <div className="page-header">
-        <h1>Sticky footer</h1>
-      </div>
-      <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-      <p>Use <a href="../sticky-footer-navbar">the sticky footer with a fixed navbar</a> if need be, too.</p>
-    </div>
-
-    <footer className="footer">
       <div className="container">
-        <p className="text-muted">Place sticky footer content here.</p>
+        <div className="page-header">
+          <h1>Ideal Portfolio Finder</h1>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="well well-lg col-xs-12 col-sm-6 col-md-6 col-lg-6">
+              <div></div>
+            </div>
+            <div className="well well-lg col-xs-12 col-sm-6 col-md-6 col-lg-6">
+              al
+            </div>
+          </div>
+        </div>
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <Panel>Look I'm in a well!
+
+                <DropdownButton />
+              </Panel>
+            </Col>
+            <Col xs={12} sm={6} md={6} lg={6}><Panel>Look I'm in a well!</Panel></Col>
+            {/* <Clearfix visibleSmBlock><code>&lt;{'Clearfix visibleSmBlock'} /&gt;</code></Clearfix>
+
+            <Col xs={12} sm={6} md={3}><Panel>Look I'm in a well!</Panel></Col>
+            <Col xs={12} sm={6} md={3}><Panel>Look I'm in a well!</Panel></Col> */}
+          </Row>
+        </Grid>
+        <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+        <p>Use <a href="../sticky-footer-navbar">the sticky footer with a fixed navbar</a> if need be, too.</p>
       </div>
-    </footer>
+
+      <hr></hr>
+      <footer className="footer">
+        <div className="container">
+          <p className="text-muted">Place sticky footer content here.</p>
+        </div>
+      </footer>
 
     </div>
   }
