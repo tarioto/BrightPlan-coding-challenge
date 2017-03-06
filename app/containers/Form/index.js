084859f461a8250d60195b21f3d6230af808e1d7
Tim Arioto <timarioto@gmail.com>
2017-03-06 09:59:08 -0800

    remove input add on

--- app/containers/Form/index.js
+++ app/containers/Form/index.js
@@ -17,28 +17,24 @@ const CategoryForm = (props) => {
         <div className="input-group">
           <span className="input-group-addon" style={{backgroundColor: '#ff7f0e', color: 'white'}}>Bonds</span>
           <Field name="bonds" component="input" type="number" placeholder="1,000,000" className="form-control"/>
-          <span className="input-group-addon">.00</span>
         </div>
       </div>
       <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon" style={{backgroundColor: '#2ca02c', color: 'white'}}>Stocks</span>
           <Field name="stocks" component="input" type="number" placeholder="1,000,000" className="form-control"/>
-          <span className="input-group-addon">.00</span>
         </div>
       </div>
       <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon" style={{backgroundColor: '#d62728', color: 'white'}}>Real Estate</span>
           <Field name="realEstate" component="input" type="number" placeholder="1,000,000" className="form-control"/>
-          <span className="input-group-addon">.00</span>
         </div>
       </div>
       <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon" style={{backgroundColor: '#9467bd', color: 'white'}}>Mutual Funds</span>
           <Field name="mutualFunds" component="input" type="number" placeholder="1,000,000" className="form-control"/>
-          <span className="input-group-addon">.00</span>
         </div>
       </div>
     </form>
