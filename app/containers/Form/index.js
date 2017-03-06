059cab86c1f44cd357ecd110b253b5f121973dc6
Tim Arioto <timarioto@gmail.com>
2017-03-05 21:32:00 -0800

    remove extra forms

--- app/containers/Form/index.js
+++ app/containers/Form/index.js
@@ -7,7 +7,7 @@ const CategoryForm = (props) => {
 
 
     <form onSubmit={handleSubmit}>
-      <div>
+      <div className="form-group">
         <label>Type the $ you have for each catigory</label>
         <div className="input-group">
           <span className="input-group-addon">Gold</span>
@@ -15,79 +15,33 @@ const CategoryForm = (props) => {
           <span className="input-group-addon">.00</span>
         </div>
       </div>
-      <div>
+      <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon">Bonds</span>
           <Field name="bonds" component="input" type="number" placeholder="1,000,000" className="form-control"/>
           <span className="input-group-addon">.00</span>
         </div>
       </div>
-      <div>
+      <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon">Stocks</span>
           <Field name="stocks" component="input" type="number" placeholder="1,000,000" className="form-control"/>
           <span className="input-group-addon">.00</span>
         </div>
       </div>
-      <div>
+      <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon">Real Estate</span>
           <Field name="realEstate" component="input" type="number" placeholder="1,000,000" className="form-control"/>
           <span className="input-group-addon">.00</span>
         </div>
       </div>
-      <div>
+      <div className="form-group">
         <div className="input-group">
           <span className="input-group-addon">Mutual Funds</span>
           <Field name="mutualFunds" component="input" type="number" placeholder="1,000,000" className="form-control"/>
           <span className="input-group-addon">.00</span>
         </div>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <div>
-          <Field name="lastName" component="input" type="text" placeholder="Last Name"/>
-        </div>
-      </div>
-      <div>
-        <label>Email</label>
-        <div>
-          <Field name="email" component="input" type="email" placeholder="Email"/>
-        </div>
-      </div>
-      <div>
-        <label>Sex</label>
-        <div>
-          <label><Field name="sex" component="input" type="radio" value="male"/> Male</label>
-          <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
-        </div>
-      </div>
-      <div>
-        <label>Favorite Color</label>
-        <div>
-          <Field name="favoriteColor" component="select">
-            <option></option>
-            <option value="ff0000">Red</option>
-            <option value="00ff00">Green</option>
-            <option value="0000ff">Blue</option>
-          </Field>
-        </div>
-      </div>
-      <div>
-        <label htmlFor="employed">Employed</label>
-        <div>
-          <Field name="employed" id="employed" component="input" type="checkbox"/>
-        </div>
-      </div>
-      <div>
-        <label>Notes</label>
-        <div>
-          <Field name="notes" component="textarea"/>
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>Submit</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
       </div>
     </form>
   )
